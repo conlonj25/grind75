7ca6aefa3d045a93d6486d3b0da4af86418ad55c
James Conlon <james.conlon@fr.fcm.travel>
2024-02-10 16:35:27 +0100

    add ex20 and ex21

---
+++ ex20/ex20.test.ts
@@ -0,0 +1,16 @@
+import { isValid } from "./ex20";
+
+type TestCase = { s: string, result: boolean }
+const TEST_CASES: TestCase[] = [
+	{ s: '()', result: true },
+	{ s: '()[]{}', result: true },
+	{ s: '(]', result: false },
+	{ s: '((()))', result: true },
+	{ s: '((()))', result: true },
+	{ s: '[', result: false },
+];
+
+
+it.each(TEST_CASES)('nums = $nums, target = $target, result = $result', ({ s, result }) => {
+	expect(isValid(s)).toBe(result);
+});
