0340f882f794a937b549f873fa44aa3c6d2dacbd
James Conlon <james.conlon@es.fcm.travel>
2024-07-21 19:59:00 +0200

    add ex70

---
+++ ex70/ex70.ts
@@ -0,0 +1,19 @@
+export function f(nums: number[]): number[][] {
+	const result: number[][] = [];
+
+	if(nums.length === 1) return [nums.slice()];
+	
+	for(let i=0; i<nums.length; i++) {
+		const n = nums.splice(0,1);
+		const perms = f(nums);
+
+		perms.forEach((perm) => {
+			result.push([...n, ...perm])
+		});
+
+		nums.push(...n);
+
+	};
+
+	return result;
+};
